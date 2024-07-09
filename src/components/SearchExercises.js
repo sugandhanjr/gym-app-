1a0ad43d99434b07537dfbd64f1d126fbd72004e
sugandhanjr <sudandhanjr@gmail.com>
2024-07-09 19:59:06 +0530

    48

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -2,6 +2,15 @@ import React, {  useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () => {
+  const [search, setsearch] = useState('')
+
+
+  const handleSearch =  async () => {
+      if(search){
+        // const exercisedata = await fetchdata();
+      }
+    }
+  
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
         <Typography fontWeight={900} sx={{ fontsize: { lg: '44px', xs: '30px'}}}
@@ -9,6 +18,43 @@ const SearchExercises = () => {
             Awesome Exercises You <br /> Should Know
         </Typography>
         <Box position=" relative" mb="72px">
+          <TextField
+          sx={{
+            input: { 
+            fontWeight: '700' , 
+            border:'none', 
+            borderRadius:'4px'
+          },
+          width: { lg: '1170px', xs: '350px'}
+          ,
+          backgroundColor: '#fff',
+          borderRadius: '40px'
+          
+          }}
+          height="76px"
+          value={search}
+          onChange= {(e) => setsearch(e.target.value.tolowercase())}
+          placeholder="Search Exercises"
+          type="text"
+          
+         
+          />
+          <Button className="search-btn"
+          sx={{
+            bgcolor: '#FF2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px'},
+            fontSize: { lg: '20px', xs: '14px'}
+            ,
+            height: '56px',
+            position: "absolute",
+            right: '0'
+          }}
+          onClick={handleSearch}
+          > 
+            Search
+          </Button>
 
         </Box>
 
